1ebf52905f8ee9dc23d1e0ba79653f90c4ae2cdd
Benjamin Hofner <benjamin.hofner@fau.de>
2015-05-06 18:40:40 +0200

    first comments, additions, changes BH

--- R/load_sea_duck_models.R
+++ R/load_sea_duck_models.R
@@ -1,27 +1,27 @@
 ## Load occupancy models for each species and rename to avoid repeatedly loading these models
-load("C:/Users/Graduate_Student/Documents/Adam's_Stuff/NanSound/Results_coei/zero.rda")
-#load("../Results_coei/zero.Rda")
+#load("C:/Users/Graduate_Student/Documents/Adam's_Stuff/NanSound/Results_coei/zero.rda")
+load("../../Code/Results_coei/zero.Rda")
 COEIocc <- zero
 
-load("C:/Users/Graduate_Student/Documents/Adam's_Stuff/NanSound/Results_scot/zero.rda")
-#load("../Results_scot/zero.Rda")
+#load("C:/Users/Graduate_Student/Documents/Adam's_Stuff/NanSound/Results_scot/zero.rda")
+load("../../Code/Results_scot/zero.Rda")
 SCOTocc <- zero
 
-load("C:/Users/Graduate_Student/Documents/Adam's_Stuff/NanSound/Results_ltdu/zero.rda")
-#load("../Results_ltdu/zero.Rda")
+#load("C:/Users/Graduate_Student/Documents/Adam's_Stuff/NanSound/Results_ltdu/zero.rda")
+load("../../Code/Results_ltdu/zero.Rda")
 LTDUocc <- zero
 
 ## Load conditional count models for each species and rename to avoid repeatedly loading these models
-load("C:/Users/Graduate_Student/Documents/Adam's_Stuff/NanSound/Results_coei/hurdle.rda")
-#load("../Results_coei/hurdle.Rda")
+#load("C:/Users/Graduate_Student/Documents/Adam's_Stuff/NanSound/Results_coei/hurdle.rda")
+load("../../Code/Results_coei/hurdle.Rda")
 COEIcc <- hurdle
 
-load("C:/Users/Graduate_Student/Documents/Adam's_Stuff/NanSound/Results_scot/hurdle.rda")
-#load("../Results_scot/hurdle.Rda")
+#load("C:/Users/Graduate_Student/Documents/Adam's_Stuff/NanSound/Results_scot/hurdle.rda")
+load("../../Code/Results_scot/hurdle.Rda")
 SCOTcc <- hurdle
 
-load("C:/Users/Graduate_Student/Documents/Adam's_Stuff/NanSound/Results_ltdu/hurdle.rda")
-#load("../Results_ltdu/hurdle.Rda")
+#load("C:/Users/Graduate_Student/Documents/Adam's_Stuff/NanSound/Results_ltdu/hurdle.rda")
+load("../../Code/Results_ltdu/hurdle.Rda")
 LTDUcc <- hurdle
 
 rm(zero); rm(hurdle)
