093419aa09afd1e0ca9e5c83a988b77fd65a1511
Adam Smith <avianmigration@gmail.com>
2015-05-07 15:50:13 -0400

    revised to be printer friendly

--- R/map_study_area.R
+++ R/map_study_area.R
@@ -20,7 +20,7 @@ detail <-
   geom_polygon(data=MA, aes(long, lat, group=group), colour = element_blank(), fill="gray85") +
   geom_path(data=transects, aes(long, lat, group=group), colour = "gray50", alpha=0.5) + 
   geom_polygon(data=seg_poly, aes(long, lat, group=group), colour = "black", alpha=0) +
-  geom_polygon(data=wind, aes(long, lat, group=group), colour="red", size = 1.5, alpha=0) +
+  geom_polygon(data=wind, aes(long, lat, group=group), colour="black", size = 1.5, alpha=0) +
   coord_equal() +
   scale_x_continuous("", limits = c(363350, 423200), expand = c(0, 0)) + 
   scale_y_continuous("", limits = c(4565650, 4625100), expand = c(0, 0)) + 
@@ -46,13 +46,13 @@ study_area <- data.frame(xmin=-70.63, xmax=-69.90, ymin=41.22, ymax=41.70)
 inset <- 
   ggplot() + 
   geom_rect(aes(xmin =-75.03, xmax = -68.55, ymin = 38.89, ymax = 45.37), fill = "white") +
-  geom_path(data=ec, aes(long, lat, group=group), colour = "black", fill="white") +
+  geom_path(data=ec, aes(long, lat, group=group), colour = "black") +
   coord_equal() +
   scale_x_continuous(limits = c(-75.03, -68.55), expand = c(0, 0)) + 
   scale_y_continuous(limits = c(38.89, 45.37), expand = c(0, 0)) +
   geom_rect(data = study_area, 
             aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), 
-            colour="red", alpha=0, size=1)  +
+            colour="black", alpha=0, size=1)  +
   theme(plot.background = element_rect(fill = "transparent", colour = NA))
   
 # Print (or save) it
@@ -60,7 +60,8 @@ inset <-
 # png(file="./Figures/Nantucket_study_area.png",w=6.5,h=6.5, res=900, units = "in")
 grid.newpage()
 v1<-viewport() #plot area for the main map
-v2<-viewport(w = 0.33, h = 0.33, x = 0, y = 1, just = c(0, 1), gp = gpar(fill=0)) #plot area for the inset map
+#plot area for the inset map
+v2<-viewport(w = 0.33, h = 0.33, x = 0, y = 1, just = c(0, 1), gp = gpar(fill=0)) 
 print(detail,vp=v1) 
 print(inset,vp=v2)
 #dev.off()
