96e506fef879f060978cb3889d4e7344fa7b6d27
Adam Smith <avianmigration@gmail.com>
2015-05-22 15:29:39 -0400

    Added functionality to get variance and thus scaled (Pearson-type) residuals from combined hurdle model

--- R/predict.gamlssHurdle.R
+++ R/predict.gamlssHurdle.R
@@ -5,18 +5,42 @@
 capture.output(gen.trun(0, family = "NBI"), file="NUL")
 
 ## See equation 6 (p. 6) in: Zeileis et al. 2008. J Stat Soft 27(8):1-25
-predict.gamlssHurdle <- function(zero, hurdle, newdata) {
+predict.gamlssHurdle <- function(zero, hurdle, newdata, 
+                                 variance = FALSE, residuals = FALSE,
+                                 append = FALSE) {
+  # variance: requests calculation of variance for fitted value
+  # residuals: calculates scaled (E(Y)/sd(Y)) Pearson-type residual for fitted value
+  # append: appends these calculations to the input data set
+  
+  if (residuals) variance <- TRUE
   
   add_pred <- predict(hurdle, newdata = newdata)
   log_p1_zero <- log(predict(zero, type = "response", newdata = newdata))
   log_p_gt_0_count <- pNBI(0, mu = exp(add_pred$mu), sigma = exp(add_pred$sigma),
                            lower.tail = FALSE, log.p = TRUE)
 
+  # Partition for ease of interpretation
+  fitted <- exp(add_pred$mu + log_p1_zero - log_p_gt_0_count)
+  mu <- exp(add_pred$mu)
+  sigma <- exp(add_pred$sigma)
+  
   # If a "count" variable is present in data, assume comparison is wanted
   if ("count" %in% colnames(newdata)) {
-    return(data.frame(obs_count = newdata$count,
-                      add_pred = add_pred$mu + log_p1_zero - log_p_gt_0_count))
+    tmpDat <- data.frame(obs_count = newdata$count,
+                         add_pred = add_pred$mu + log_p1_zero - log_p_gt_0_count)
   } else {
-    return(data.frame(add_pred = add_pred$mu + log_p1_zero - log_p_gt_0_count))
+    if (residuals) stop("Can't calculate residuals without an observed 'count' variable")
+    tmpDat <- data.frame(add_pred = add_pred$mu + log_p1_zero - log_p_gt_0_count)
   }
+  
+  if (variance) {
+    tmpDat$variance <- fitted * (1 + mu + sigma * mu - fitted)
+  }
+  
+  if (residuals) tmpDat$scaled_resid <- with(tmpDat, (obs_count - exp(add_pred)) / sqrt(variance))
+  
+  if (append) tmpDat <- data.frame(newdata, tmpDat[, -1])
+  
+  return(tmpDat)
+
 }
