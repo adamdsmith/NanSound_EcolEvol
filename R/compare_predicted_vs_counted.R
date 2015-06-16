767c51323271816af13a092fd2d317b719549ba7
Adam Smith <avianmigration@gmail.com>
2015-06-16 11:48:14 -0400

    Changed Fig 6 to log-log and added color

--- R/compare_predicted_vs_counted.R
+++ R/compare_predicted_vs_counted.R
@@ -34,20 +34,23 @@ ltdu_compare <- left_join(ltdu_obs, ltdu_test)
 
 all_ducks <- data.frame(spp = rep(c("COEI", "SCOT", "LTDU"), each=30),
                       rbind(coei_compare, scot_compare, ltdu_compare))
+all_ducks$spp <- factor(all_ducks$spp, levels = c("COEI", "SCOT", "LTDU"))
 
 #ggplot(all_ducks, aes(x=log(est_count), y=log(total_count))) + 
 p <- ggplot(all_ducks, aes(x=total_count, y=est_count)) +
   coord_equal() +
-  geom_point(aes(group=spp, shape = spp, size=spp)) + 
+  geom_point(aes(group=spp, shape = spp, size=spp, fill = spp), alpha=0.75) + 
+  scale_fill_brewer(type = "qual", palette="Set2", guide = guide_legend(title = NULL)) +
   scale_shape_manual(values = c(21, 22, 24), guide = guide_legend(title = NULL)) +
   scale_size_manual(values = c(2,2,1.75), guide = guide_legend(title = NULL)) +
   geom_abline(intercept=0, slope=1, linetype="dashed") + 
   theme_classic() +
-  scale_x_continuous("Observed abundance") + scale_y_continuous("Predcited abundance") +
-  theme(legend.position = c(1, 1), legend.justification = c(1, 0.75),
+  scale_x_log10("Observed abundance", limits=c(50,10^5), breaks=10^(1:5)) + 
+  scale_y_log10("Predicted abundance", limits=c(50,10^5), breaks=10^(1:5)) +
+  theme(legend.position = c(0, 1), legend.justification = c(0.1, 0.75),
         legend.direction = "vertical")
 
 ## CREATE FILE
-#png(file = "./Figures/Predicted_abundance_observed_abundance.png", height = 6.5/1.38753, width = 6.5, units = "in", res = 600)
+#png(file = "./Figures/Predicted_abundance_observed_abundance.png", height = 5.5/1.38753, width = 5.5, units = "in", res = 600)
 p
 #dev.off()
